9974fe906dc2756a20eae790297f9cb9b755990a
DjordjeZlatanovic <zlatanovic.basketball@gmail.com>
2024-11-07 12:20:42 +0100

    izmena gredla

--- excel/build.gradle.kts
+++ excel/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation(project(":Spec"))
     implementation("org.apache.poi:poi:5.3.0")
-    implementation ("org.apache.poi:poi-ooxml:5.2.3")
-    implementation ("org.apache.xmlbeans:xmlbeans:5.0.2")
+    implementation("org.apache.poi:poi-ooxml:5.3.0")
+    implementation("org.apache.xmlbeans:xmlbeans:5.0.2")
 
 
 
